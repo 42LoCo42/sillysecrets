1e034d701640bcdef5adfe5ac8e1d4c2a2b8589a
Leon Schumacher <leonsch@protonmail.com>
2024-12-01 16:41:12 +0100

    add collectors; remove duplicate code

--- lib_test.go
+++ lib_test.go
@@ -5,15 +5,8 @@ import (
 	"testing"
 
 	"github.com/42LoCo42/sillysecrets"
+	set "github.com/deckarep/golang-set/v2"
 )
-
-func Must[X any](x X, err error) X {
-	if err != nil {
-		log.Panic(err)
-	}
-
-	return x
-}
 
 func Test(t *testing.T) {
 	groups, err := sillysecrets.Load("example/sesi.yaml")
@@ -22,14 +15,22 @@ func Test(t *testing.T) {
 	}
 
 	log.Print("ResolveToContains")
-	contains_want := Must(sillysecrets.Load("example/sesi.contains.yaml"))
-	contains_have, err := sillysecrets.ResolveToContains(groups)
-	if err != nil {
-		t.Fatal(t)
-	}
-	for n := range contains_have {
-		have := contains_have[n].Contains
-		want := contains_want[n].Contains
+	for n, g := range groups {
+		have := g.Contains
+
+		want := map[string]set.Set[string]{
+			"admin:alice": set.NewSet(
+				"machine:lazuli",
+			),
+			"machine:lazuli": set.NewSet(
+				"user:bob",
+			),
+			"user:bob": set.NewSet[string](),
+			"user:friend": set.NewSet(
+				"user:bob",
+			),
+		}[n]
+
 		log.Printf("  %v: have %v, want %v", n, have, want)
 		if !have.Equal(want) {
 			t.Fatal(have, want)
@@ -37,14 +38,82 @@ func Test(t *testing.T) {
 	}
 
 	log.Print("ResolveToGrants")
-	grants_want := Must(sillysecrets.Load("example/sesi.grants.yaml"))
-	grants_have, err := sillysecrets.ResolveToGrants(groups)
-	if err != nil {
-		t.Fatal(t)
+	for n, g := range groups {
+		have := g.Grants
+
+		want := map[string]set.Set[string]{
+			"admin:alice": set.NewSet[string](),
+			"machine:lazuli": set.NewSet(
+				"admin:alice",
+			),
+			"user:bob": set.NewSet(
+				"machine:lazuli",
+				"user:friend",
+			),
+			"user:friend": set.NewSet[string](),
+		}[n]
+
+		log.Printf("  %v: have %v, want %v", n, have, want)
+		if !have.Equal(want) {
+			t.Fatal(have, want)
+		}
 	}
-	for n := range grants_have {
-		have := grants_have[n].Grants
-		want := grants_want[n].Grants
+
+	log.Print("CollectSecrets")
+	for n := range groups {
+		have, err := sillysecrets.CollectSecrets(n, groups)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		want := map[string]set.Set[string]{
+			"admin:alice": set.NewSet(
+				"machine:lazuli.example",
+				"user:bob.password",
+			),
+			"machine:lazuli": set.NewSet(
+				"machine:lazuli.example",
+				"user:bob.password",
+			),
+			"user:bob": set.NewSet(
+				"user:bob.password",
+			),
+			"user:friend": set.NewSet(
+				"user:bob.password",
+			),
+		}[n]
+
+		log.Printf("  %v: have %v, want %v", n, have, want)
+		if !have.Equal(want) {
+			t.Fatal(have, want)
+		}
+	}
+
+	log.Print("CollectKeys")
+	for n := range groups {
+		have, err := sillysecrets.CollectKeys(n, groups)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		want := map[string]set.Set[string]{
+			"admin:alice": set.NewSet(
+				groups["admin:alice"].Key,
+			),
+			"machine:lazuli": set.NewSet(
+				groups["admin:alice"].Key,
+				groups["machine:lazuli"].Key,
+			),
+			"user:bob": set.NewSet(
+				groups["admin:alice"].Key,
+				groups["machine:lazuli"].Key,
+				groups["user:friend"].Key,
+			),
+			"user:friend": set.NewSet(
+				groups["user:friend"].Key,
+			),
+		}[n]
+
 		log.Printf("  %v: have %v, want %v", n, have, want)
 		if !have.Equal(want) {
 			t.Fatal(have, want)
